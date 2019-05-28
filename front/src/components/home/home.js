d12c04280a9c9e36ab4c91ae1cec7a095ce3cb9a
yahiaDev <yahia.ammar.info@gmail.com>
2019-05-28 11:43:43 +0200

    front: add book

--- front/src/components/home/home.js
+++ front/src/components/home/home.js
@@ -8,7 +8,10 @@ export default {
     return {
       books: [],
       bookTitle: '',
-      selectedBooksData: []
+      selectedBooksData: [],
+      newBook: {title: '', description: ''},
+      bookTitleState: null,
+      bookDescriptionState: null
     }
   },
   computed: {
@@ -36,7 +39,47 @@ export default {
 
   },
   methods: {
-    
+    showAddBookModal() {
+      this.$refs['add-book-modal'].show();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
+      }
+
+      console.log('book title is' + this.newBook.title);
+      
+
+      axios.post("http://localhost:8090/book/add", this.newBook).then(response => { 
+        /* eslint-disable */ 
+        console.log(response.data);
+        this.books = response.data;
+       });
+
+      // Hide the modal manually
+      this.$nextTick(() => {
+        // this.$refs.modal.hide();
+        this.$refs['add-book-modal'].hide();
+      });
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    checkFormValidity() {
+      this.bookTitleState  = this.newBook.title === '' ? 'invalid': 'valid';
+      this.bookDescriptionState  = this.newBook.title === '' ? 'invalid': 'valid';
+      return this.newBook.title !== '' && this.newBook.title !== '';
+    },
+    resetModal() {
+      this.newBook.title = '';
+      this.newBook.description = '';
+      this.bookTitleState = null;
+      this.bookDescriptionState  = null;
+    },
 
   }
 }
